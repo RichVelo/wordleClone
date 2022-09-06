830130b09480db22d22c88b35be1caa2b554e566
RichVelo <richrar@gmail.com>
2022-09-06 16:23:00 +0100

    keyboard and some script

---
+++ app.js
@@ -0,0 +1,62 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyBoard = document.querySelector('.key-container')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
+
+const guessRows = [
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','',''],
+    ['','','','','']
+]
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+
+    tileDisplay.append(rowElement)
+})
+
+const handleClick = () => {
+    console.log('clicked')
+}
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', () => handleClick(key))
+    keyBoard.append(buttonElement)
+})
+
